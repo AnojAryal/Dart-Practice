2e13ac9b7c8f10f1ba549ee21e3adcc64659f30c
anojaryal <anoj1810@gmail.com>
2024-04-04 17:18:53 +0545

    dart basics

---
+++ variables.dart
@@ -0,0 +1,21 @@
+void main() {
+  var name = 'FraNzY';
+  print(name);
+
+  final age = 21;
+  print (age);
+
+  const isOpen = true;
+  print (isOpen);
+
+  print(age + 10);
+  print(age - 10);
+  print(age * 10);
+  print(age / 10);
+  print(age % 10);
+
+  print('My name is ' + name);
+
+
+  print('My name is $name and my age is $age');
+}
