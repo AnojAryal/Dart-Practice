3b9d9fa3da114bd944b4356df9667c1082a3bf18
anojaryal <anoj1810@gmail.com>
2024-04-04 17:34:42 +0545

    type annotation in dart

--- type-annotations.dart
+++ type-annotations.dart
@@ -1,5 +1,19 @@
 void main() {
- String name = 'Kriti';
-
+ const String name = 'Kriti';
  print (name);
+
+ const int age = 23;
+ print (age);
+
+ bool isClosed = false;
+ isClosed = true;
+ print (isClosed);
+
+ double averageRating = 7.9;
+ averageRating = 8.3;
+ print (averageRating);
+
+ //declaring the null variables
+ int? point;
+ print (point);
 }
