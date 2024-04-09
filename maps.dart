70055f683286a6541c25146f30a1f20b68cc50fc
anojaryal <anoj1810@gmail.com>
2024-04-09 19:47:09 +0545

    maps in dart

--- maps.dart
+++ maps.dart
@@ -1,6 +1,24 @@
 void main() {
- 
- planets = {};
+  Map<String, String> planets = {
+    'First': 'Mercury',
+    'Second': 'Venus',
+    'Third': 'Earth',
+    'Fourth': 'Mars',
+    'Fifth': 'Jupiter',
+    'Sixth': 'Saturn',
+    'Seventh': 'Uranus',
+    'Eighth': 'Neptune',
+    'Ninth': 'Pluto',
+  };
 
- 
+  planets.forEach((ordinal, name) {
+    print('$ordinal planet is $name');
+  });
+
+  print(planets.containsKey('Third'));
+  print(planets.containsValue('Earth'));
+  print(planets.keys);
+  print(planets.values);
+
+  print(planets);
 }
