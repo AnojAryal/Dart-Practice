af722d95cb55c6d34e6cab45f55e74a7902ec6b2
anojaryal <anoj1810@gmail.com>
2024-04-10 21:24:59 +0545

    Fix constructor call in Pizza class and add missing super parameter

--- classes.dart
+++ classes.dart
@@ -1,13 +1,16 @@
 void main() {
   
   var noodles = MenuItem('Chicken noodle', 4.99);
-  var pizza = MenuItem('Cheese Pizza', 5.99);
+  var pizza = Pizza(['mushrooms', 'peppers'],'Cheese Pizza', 5.99);
   
   print (noodles.title);
   print (noodles.price);
  
   print (pizza.title);
   print (pizza.price); 
+  
+  print (noodles.format());
+  print (pizza.format());
 }
 
 class MenuItem {
@@ -15,4 +18,15 @@ class MenuItem {
   double price;
   
   MenuItem(this.title, this.price);
+  
+  String format() {
+    return ('$title --> $price');
+  }
+
+}
+
+class Pizza extends MenuItem{
+  List<String> toppings;
+  
+  Pizza(this.toppings, super.title, super.price);
 }
