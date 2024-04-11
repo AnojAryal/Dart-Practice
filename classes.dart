bda94926e5672f5abfcb4d9827139dedd8f3a558
anojaryal <anoj1810@gmail.com>
2024-04-11 20:56:17 +0545

    method override i dart

--- classes.dart
+++ classes.dart
@@ -1,42 +1,45 @@
 void main() {
-  
   var noodles = MenuItem('Chicken noodle', 4.99);
-  var pizza = Pizza(['mushrooms', 'peppers'],'Cheese Pizza', 5.99);
-  
-  print (noodles.title);
-  print (noodles.price);
- 
-  print (pizza.title);
-  print (pizza.price); 
-  
-  print (noodles.format());
-  print (pizza.format());
+  var pizza = Pizza(['mushrooms', 'peppers'], 'Cheese Pizza', 5.99);
+
+  print(noodles.title);
+  print(noodles.price);
+
+  print(pizza.title);
+  print(pizza.price);
+
+  print(noodles);
+  print(pizza);
 }
 
 class MenuItem {
   String title;
   double price;
-  
+
   MenuItem(this.title, this.price);
-  
+
   String format() {
     return ('$title --> $price');
   }
 
+  @override
+  String toString() {
+    return format();
+  }
 }
 
-class Pizza extends MenuItem{
+class Pizza extends MenuItem {
   List<String> toppings;
-  
+
   Pizza(this.toppings, super.title, super.price);
-  
+
   @override
-  String format () {
+  String format() {
     var formattedToppings = 'Contains:';
-    
+
     for (final t in toppings) {
-      
       formattedToppings = '$formattedToppings $t';
-}
+    }
     return '$title --> $price \n$formattedToppings';
   }
+}
