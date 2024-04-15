cee21d99dda3c36cd626160aadbc822d46ca3c44
anojaryal <anoj1810@gmail.com>
2024-04-15 18:11:11 +0545

    using http package

---
+++ fetching-data.dart
@@ -0,0 +1,23 @@
+import 'package:http/http.dart' as http;
+
+void main() async {
+  final post = await fetchpost();
+  print(post.title);
+  print(post.userId);
+
+} 
+
+Future<Post> fetchpost() {
+  const delay = Duration(seconds: 3);
+
+  return Future.delayed(delay,(){
+    return Post('I am FraNzY', 18);
+  });
+}
+
+class Post{
+  String title;
+  int userId;
+
+  Post(this.title, this.userId);
+}
